2961ffea011b2e420eb3b1979a038b0a4a79eeaa
sebavue <seba.vue@gmail.com>
2022-11-08 23:23:36 +0100

    Finished L8 Lesson

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      onSale: false,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/images/socks_green.jpg',
+          quantity: 50,
+          onSale: false,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+          onSale: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+      this.onSale = this.variants[this.selectedVariant].onSale;
+    },
+  },
+  computed: {
+    title() {
+      title = `${this.brand} ${this.product}`;
+      if (this.onSale) {
+        title += ' is on Sale';
+      }
+      return title;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+  },
+});
